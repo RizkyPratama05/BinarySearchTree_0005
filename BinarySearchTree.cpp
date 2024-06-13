2dc10d62aaac718c9df63efd78fd7841fedb78ff
RizkyPratama05 <rizkypp428@gmail.com>
2024-06-13 20:34:39 +0700

    void inorder

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -20,6 +20,7 @@ class Node
 
 class BinaryTree
 {
+public:
     Node* ROOT;
 
     BinaryTree()
@@ -53,4 +54,34 @@ class BinaryTree
             parent->rightchild = newNode;
         }
     }
-};
+
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        currentNode = ROOT;
+        parent = NULL;
+        while ((currentNode != NULL) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+    void inorder(Node* ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr !=NULL)
+        {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+  
