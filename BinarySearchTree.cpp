26b78dbd94e93d0755adafc4601a37c007f324b3
RizkyPratama05 <rizkypp428@gmail.com>
2024-06-13 20:38:54 +0700

    case 2

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -142,4 +142,9 @@ int main()
                 obj.insert(word);
                 break;
             }
+          case '2':
+            {
+                obj.inorder(obj.ROOT);
+                break;
+            }
           
